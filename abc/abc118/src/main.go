745ca3ddc9c99e523d5fca1195070b29c90811c3
shihou22 <pc9801bx2@gmail.com>
2019-03-02 22:39:16 +0900

    Add

---
+++ abc/abc118/src/main.go
@@ -0,0 +1,16 @@
+package main
+
+import "fmt"
+
+func main() {
+	// var x, a, b int
+	// fmt.Scan(&x, &a, &b)
+	// res := hoge(x, a, b)
+	fmt.Println("abc118 - main")
+}
+
+func hoge(x int, a int, b int) int {
+	var ans int
+	ans = (x - a) % b
+	return ans
+}
