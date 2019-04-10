02a2adb79408398d8cec21059c3d7c35b23c413c
shihou22 <pc9801bx2@gmail.com>
2019-04-10 16:27:07 +0900

    add abc120 go-lang

--- abc/abc118/src/main.go
+++ abc/abc118/src/main.go
@@ -2,6 +2,7 @@ package main
 
 import (
 	"fmt"
+	"math"
 	"time"
 )
 
@@ -20,16 +21,31 @@ func main() {
 
 	//solveB
 	{
-		var n int
-		fmt.Scan(&n)
-		a := make([]float64, n)
-		b := make([]string, n)
-		for i := 0; i < n; i++ {
-			fmt.Scan(&a[i])
-			fmt.Scan(&b[i])
+		// var n int
+		// fmt.Scan(&n)
+		// a := make([]float64, n)
+		// b := make([]string, n)
+		// for i := 0; i < n; i++ {
+		// 	fmt.Scan(&a[i])
+		// 	fmt.Scan(&b[i])
+		// }
+		// res := solveB(a, b)
+		// fmt.Println(res)
+	}
+
+	//solveC
+	{
+
+		fmt.Scan(&nC)
+		fmt.Scan(&aC)
+		fmt.Scan(&bC)
+		fmt.Scan(&cC)
+		wk := make([]int, nC)
+		for i := 0; i < nC; i++ {
+			fmt.Scan(&wk[i])
 		}
-		res := solveB(a, b)
-		fmt.Println(res)
+		res := solveC(wk)
+		fmt.Println(int(res))
 	}
 }
 
@@ -56,4 +72,28 @@ func solveB(a []float64, b []string) float64 {
 		}
 	}
 	return total
+}
+
+var nC, aC, bC, cC int
+
+func solveC(wk []int) float64 {
+
+	return dfsC(0, 0, 0, 0, wk)
+
+}
+
+func dfsC(currentI int, a int, b int, c int, wk []int) float64 {
+
+	if nC == currentI {
+		if a > 0 && b > 0 && c > 0 {
+			return float64(math.Abs(float64(aC-a) + math.Abs(float64(bC-b)+math.Abs(float64(cC-c)-30))))
+		}
+		return 99999999
+	}
+	val0 := dfsC(currentI+1, a, b, c, wk)
+	val1 := dfsC(currentI+1, a+wk[currentI], b, c, wk) + 10
+	val2 := dfsC(currentI+1, a, b+wk[currentI], c, wk) + 10
+	val3 := dfsC(currentI+1, a, b, c+wk[currentI], wk) + 10
+
+	return math.Min(math.Min(val0, val1), math.Min(val2, val3))
 }
