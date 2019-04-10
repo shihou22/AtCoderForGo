7663ac04e8848690406d3f800b48f8079523f312
shihou22 <pc9801bx2@gmail.com>
2019-04-10 17:07:01 +0900

    abc118 a go

--- abc/abc118/src/main.go
+++ abc/abc118/src/main.go
@@ -1,99 +1,27 @@
 package main
 
-import (
-	"fmt"
-	"math"
-	"time"
-)
-
-const (
-	yyyymmdd = "2006/01/02"
-)
+import "fmt"
 
 func main() {
-	// solveA
-	{
-		// var dateS, resA string
-		// _, _ = fmt.Scan(&dateS)
-		// resA = solveA(dateS)
-		// fmt.Println(resA)
-	}
 
-	//solveB
-	{
-		// var n int
-		// fmt.Scan(&n)
-		// a := make([]float64, n)
-		// b := make([]string, n)
-		// for i := 0; i < n; i++ {
-		// 	fmt.Scan(&a[i])
-		// 	fmt.Scan(&b[i])
-		// }
-		// res := solveB(a, b)
-		// fmt.Println(res)
-	}
+	mainA()
 
-	//solveC
-	{
-
-		fmt.Scan(&nC)
-		fmt.Scan(&aC)
-		fmt.Scan(&bC)
-		fmt.Scan(&cC)
-		wk := make([]int, nC)
-		for i := 0; i < nC; i++ {
-			fmt.Scan(&wk[i])
-		}
-		res := solveC(wk)
-		fmt.Println(int(res))
-	}
 }
 
-func solveA(dateS string) string {
+func mainA() {
+	var a, b, res int64
+	fmt.Scan(&a, &b)
+	res = solveA(a, b)
+	fmt.Println(res)
+}
 
-	t1, _ := time.Parse(yyyymmdd, dateS)
-	t2, _ := time.Parse(yyyymmdd, "2019/04/30")
-
-	if t1.Unix() <= t2.Unix() {
-		return "Heisei"
+func solveA(a int64, b int64) int64 {
+	var res int64
+	if b%a == 0 {
+		res = a + b
 	} else {
-		return "TBD"
+		res = b - a
 	}
-}
 
-func solveB(a []float64, b []string) float64 {
-
-	total := 0.0
-	for index := 0; index < len(a); index++ {
-		if b[index] == "JPY" {
-			total += a[index]
-		} else if b[index] == "BTC" {
-			total += a[index] * 380000.0
-		}
-	}
-	return total
-}
-
-var nC, aC, bC, cC int
-
-func solveC(wk []int) float64 {
-
-	return dfsC(0, 0, 0, 0, wk)
-
-}
-
-func dfsC(currentI int, a int, b int, c int, wk []int) float64 {
-
-	if nC == currentI {
-		if a > 0 && b > 0 && c > 0 {
-			return float64(math.Abs(float64(aC-a) + math.Abs(float64(bC-b)+math.Abs(float64(cC-c)-30))))
-		}
-		return 99999999
-	}
-	val0 := dfsC(currentI+1, a, b, c, wk)
-	val1 := dfsC(currentI+1, a+wk[currentI], b, c, wk) + 10
-	val2 := dfsC(currentI+1, a, b+wk[currentI], c, wk) + 10
-	val3 := dfsC(currentI+1, a, b, c+wk[currentI], wk) + 10
-
-	return math.Min(math.Min(val0, val1), math.Min(val2, val3))
+	return res
 }
--- abc/abc118/src/main_test.go
+++ abc/abc118/src/main_test.go
@@ -1,47 +1,25 @@
 package main
 
-import (
-	"testing"
-)
+import "testing"
 
 func Test_solveA(t *testing.T) {
 	type args struct {
-		dateS string
+		a int64
+		b int64
 	}
 	tests := []struct {
 		name string
 		args args
-		want string
+		want int64
 	}{
-		{name: "1", args: args{"2019/04/30"}, want: "Heisei"},
-		{name: "2", args: args{"2019/11/01"}, want: "TBD"},
+		{name: "1", args: args{a: 4, b: 12}, want: 16},
+		{name: "2", args: args{a: 8, b: 20}, want: 12},
+		{name: "3", args: args{a: 1, b: 1}, want: 2},
 	}
 	for _, tt := range tests {
 		t.Run(tt.name, func(t *testing.T) {
-			if got := solveA(tt.args.dateS); got != tt.want {
+			if got := solveA(tt.args.a, tt.args.b); got != tt.want {
 				t.Errorf("solveA() = %v, want %v", got, tt.want)
-			}
-		})
-	}
-}
-
-func Test_solveB(t *testing.T) {
-	type args struct {
-		a []float64
-		b []string
-	}
-	tests := []struct {
-		name string
-		args args
-		want float64
-	}{
-		{name: "1", args: args{a: []float64{10000, 0.10000000}, b: []string{"JPY", "BTC"}}, want: 48000.0},
-		{name: "2", args: args{a: []float64{100000000, 100.00000000, 0.00000001}, b: []string{"JPY", "BTC", "BTC"}}, want: 138000000.0038},
-	}
-	for _, tt := range tests {
-		t.Run(tt.name, func(t *testing.T) {
-			if got := solveB(tt.args.a, tt.args.b); got != tt.want {
-				t.Errorf("solveB() = %v, want %v", got, tt.want)
 			}
 		})
 	}
