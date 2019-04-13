bc4fd2d355371f6070e5aad1b23fd2a0448973cb
shihou22 <pc9801bx2@gmail.com>
2019-04-13 20:18:36 +0900

    abc041 A&B&C

--- src/abc/abc041/main.go
+++ src/abc/abc041/main.go
@@ -2,11 +2,16 @@ package main
 
 import (
 	"fmt"
+	"math"
+	"sort"
+	"strconv"
 )
 
 func main() {
 
-	mainA()
+	// mainA()
+	// mainB()
+	mainC()
 }
 
 func mainA() {
@@ -19,4 +24,66 @@ func mainA() {
 
 func solveA(s string, numI int) string {
 	return s[numI-1 : numI]
+}
+
+func mainB() {
+
+	var a, b, c int64
+	fmt.Scan(&a, &b, &c)
+	var res = solveB(a, b, c)
+
+	fmt.Println(res)
+}
+
+func solveB(a, b, c int64) int64 {
+	var ConstNum = int64(math.Pow(10, 9) + 7)
+	return a * b % ConstNum * c % ConstNum
+}
+
+//privateで構造体宣言
+type StudentC struct {
+	Index  int
+	Height int
+}
+
+func mainC() {
+	var n int
+	var wk []StudentC
+	fmt.Scan(&n)
+
+	wk = make([]StudentC, n)
+
+	for i := 0; i < n; i++ {
+		var b int
+		fmt.Scan(&b)
+		wk[i].Index = i + 1
+		wk[i].Height = b
+		// wk[i] = StudentC[Index:i+1, Height:b]
+	}
+
+	var res = solveC(n, wk)
+	for i := 0; i < len(wk); i++ {
+		fmt.Println(res[i])
+	}
+}
+
+/*
+テストのために、fmt.Printlnで直接出力するのではなく
+[]stringに詰めてreturnしている
+*/
+func solveC(n int, wk []StudentC) []string {
+	/*
+		構造体のsliceをsortする
+	*/
+	sort.SliceStable(wk, func(i, j int) bool {
+		return wk[i].Height > wk[j].Height
+	})
+	res := make([]string, 0)
+	for i := 0; i < len(wk); i++ {
+		/*
+			strconvで数値を文字列へ
+		*/
+		res = append(res, strconv.Itoa(wk[i].Index))
+	}
+	return res
 }
--- src/abc/abc041/main_test.go
+++ src/abc/abc041/main_test.go
@@ -1,6 +1,9 @@
 package main
 
-import "testing"
+import (
+	"reflect"
+	"testing"
+)
 
 func Test_solveA(t *testing.T) {
 	type args struct {
@@ -20,6 +23,52 @@ func Test_solveA(t *testing.T) {
 		t.Run(tt.name, func(t *testing.T) {
 			if got := solveA(tt.args.s, tt.args.numI); got != tt.want {
 				t.Errorf("solveA() = %v, want %v", got, tt.want)
+			}
+		})
+	}
+}
+
+func Test_solveB(t *testing.T) {
+	type args struct {
+		a int64
+		b int64
+		c int64
+	}
+	tests := []struct {
+		name string
+		args args
+		want int64
+	}{
+		{name: "", args: args{2, 3, 4}, want: 24},
+		{name: "", args: args{10000, 1000, 100}, want: 1000000000},
+		{name: "", args: args{100000, 1, 100000}, want: 999999937},
+		{name: "", args: args{1000000000, 1000000000, 1000000000}, want: 999999664},
+	}
+	for _, tt := range tests {
+		t.Run(tt.name, func(t *testing.T) {
+			if got := solveB(tt.args.a, tt.args.b, tt.args.c); got != tt.want {
+				t.Errorf("solveB() = %v, want %v", got, tt.want)
+			}
+		})
+	}
+}
+
+func Test_solveC(t *testing.T) {
+	type args struct {
+		n  int
+		wk []StudentC
+	}
+	tests := []struct {
+		name string
+		args args
+		want []string
+	}{
+		{name: "", args: args{n: 3, wk: []StudentC{{Index: 1, Height: 140}, {Index: 2, Height: 180}, {Index: 3, Height: 160}}}, want: []string{"2", "3", "1"}},
+	}
+	for _, tt := range tests {
+		t.Run(tt.name, func(t *testing.T) {
+			if got := solveC(tt.args.n, tt.args.wk); !reflect.DeepEqual(got, tt.want) {
+				t.Errorf("solveC() = %v, want %v", got, tt.want)
 			}
 		})
 	}
